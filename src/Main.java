99045cef082d1425a6cda1400d0b9e7844271f51
Fiona Tolliday <fionatolliday@hotmail.com>
2019-09-09 11:14:10 +1000

    test

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
          HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
             server.createContext("/greeting", new GreetingHandler(world));
             server.createContext("/names", new PersonHandler(world));
-            server.setExecutor(null); // creates a default executor. this submits a new task
+            server.setExecutor(null); // creates a default executor. this submits a new tasks
             server.start();
         }
 
