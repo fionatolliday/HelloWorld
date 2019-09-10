1d73ff4831fa8aa568641ec0a6b5011e9e357dd7
Fiona Tolliday <fionatolliday@hotmail.com>
2019-09-10 15:40:57 +1000

    buildkite.yml file change

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,4 +1,4 @@
-docker build -t app .
-docker tag fiona-hello-world:latest 945367126992.dkr.ecr.ap-southeast-2.amazonaws
+- docker build -t app .
+- docker tag fiona-hello-world:latest 945367126992.dkr.ecr.ap-southeast-2.amazonaws
 .com/fiona-hello-world:latest
-docker push 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/fiona-hello-world:latest
+- docker push 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/fiona-hello-world:latest
