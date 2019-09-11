e19e5fa7d4395cd038628e49024a4b153b5c8ad7
Fiona Tolliday <fionatolliday@hotmail.com>
2019-09-12 08:34:59 +1000

    added ECR login

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,3 +1,4 @@
 docker build -t app .
 docker tag fiona-hello-world:latest 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/fiona-hello-world:latest
+$(aws ecr get-login --no-include-email --region ap-southeast-2)
 docker push 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/fiona-hello-world:latest
